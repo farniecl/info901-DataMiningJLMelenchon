c40ff7905c5491ce418bfefd775a4baf22a56a27
ubron974 <brunolauret7@gmail.com>
2017-03-05 01:49:46 +0100

    Classifieur + stream

--- main/main.py
+++ main/main.py
@@ -10,34 +10,45 @@ import json
 from pprint import pprint
 from twitter.stream_tweet import StreamTweet
 from twitter.classifieur import Classifieur
-if __name__ == '__main__':
-    # Cles et Access Token pour se connecter à twitter
+
+json_file = './ressource/tweets.json'
+
+def update_json(tweets):
+    fr = open(json_file,'r')
+    file_tweets = json.load(fr)
+    fr.close()
+    file_tweets.extend(tweets)
+    fw = open(json_file, 'w')
+    fw.write(json.dumps(file_tweets,indent=4,separators=(',',':')))
+    fw.close()
+
+def stream_tweets(tweets):
+     # Cles et Access Token pour se connecter à twitter
     access_token = "517636552-0dHeUg75oHdKmLaO2RjFjKlSF5ZJQtcDW7tHfLl3"
     access_token_secret = "QoCIwz8ziMj7NABzxRQqQHGuHlDIdXppJJYdzpV001aWj"
     consumer_key = "Xsd9vLsEmQLqqswUrt0fTOYun"
     consumer_secret = "kIinFrugN04pqNG2DfonwAylbzNmjzFpGVAdwBDxEca1S975Q2"
     #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StreamTweet("ressource/tweets.json",time_limit=20)
+    l = StreamTweet(time_limit=20)
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
     stream.filter(track=['@JLMelenchon'])
-    with open("ressource/tweets.json", 'r') as f:
-        tweets = json.load(f)
-        for tweet in tweets:
-            pprint(tweet["text"])
-        #test = Classifieur()
-        #test.init_classifier()
-        #test.apprentissage()
-    #for line in f:
-    #tweets.append(json.loads(line))
-    #f = json.load(open("ressource/tweets.json").read())
-    #test = Classifieur()
-    #test.init_classifier(),indent=4, separators=(',',,indent=4, separators=(',',':'),indent=4, separators=(',',':')':')
-    #for tweet in f:
-    #print(tweet.text)   
-    #f.close()
-    #tweet = 'Bruno Lauret'
-    #print (analyse(tweet))
+    tweets.extend(l.get_tweets())
+
+def pos_neg_app(tweets):
+    cl = Classifieur('./ressource/tweets.json')
+    for tweet in tweets:
+        tweet["sentiment"] = cl.apprentissage(tweet["tweet"])
+
+def pos_neg_ana(tweets):
+    cl = Classifieur('./ressource/tweets.json')
+    for tweet in tweets:
+        tweet["sentiment"] = cl.analyse(tweet["tweet"])
+
+if __name__ == '__main__':
+    tweets = []
+    stream_tweets(tweets)
+    pos_neg_app(tweets)
+    update_json(tweets)
--- main/twitter/stream_tweet.py
+++ main/twitter/stream_tweet.py
@@ -10,21 +10,24 @@ import json
 
 # Méthode qui lit une donnée reçue et l'affiche dans la console
 class StreamTweet(StreamListener):
-    def __init__(self,path,time_limit=60):
-        self.start_time = time.time()
-        self.limit = time_limit
-        self.saveFile = open(path, 'a')
-        self.tweets = []
+    def __init__(self,time_limit=60):
+        self.__start_time = time.time()
+        self.__limit = time_limit
+        self.__tweets = []
         super(StreamTweet, self).__init__()
 
     def on_data(self, data):
-        if (time.time() - self.start_time) < self.limit:
-            self.tweets.append(json.loads(data))
+        if (time.time() - self.__start_time) < self.__limit:
+            self.__tweets.append(json.loads(data))
             return True
         else:
-            self.saveFile.write(json.dumps(self.tweets,indent=4, separators=(',',':')))
-            self.saveFile.close()
             return False
 
     def on_error(self, status):
         print(status)
+
+    def get_tweets(self):
+        tmp_tweets = []
+        for tweet in self.__tweets:
+            tmp_tweets.append({"tweet":tweet["text"]})
+        return tmp_tweets
